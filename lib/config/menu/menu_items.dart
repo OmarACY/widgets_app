f328c48de90e8fbceeff8cbf124ce582591e1575
OmarACY <omaralejandrocy@gmail.com>
2024-11-22 23:10:54 -0600

    138. Snackbars

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -13,5 +13,6 @@ const appMenuItems = <MenuItem>[
   MenuItem('Botones', 'Varios botones en Flutter', '/buttons',
       Icons.smart_button_outlined),
   MenuItem('Tarjetas', 'Un contenedor estilizado', '/cards', Icons.credit_card),
-  MenuItem('ProgressIndicators', 'Generales y controlados', '/progress', Icons.refresh_rounded)
+  MenuItem('Progress Indicators', 'Generales y controlados', '/progress', Icons.refresh_rounded),
+  MenuItem('Snackbars y diálogos', 'Indicadores en pantalla', '/snackbars', Icons.info_outlined)
 ];
--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -6,8 +6,81 @@ class SnackbarScreen extends StatelessWidget {
 
   const SnackbarScreen({super.key});
 
+  void _showCustomSnackbar(BuildContext context) {
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    var snackbar = SnackBar(
+        content: const Text('Hola mundo!'),
+        duration: const Duration(seconds: 2),
+        action: SnackBarAction(label: 'Ok!',onPressed: () {},
+        ),
+      );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbars y diálogos'),
+      ),
+      body: const _SnackbarView(),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => _showCustomSnackbar(context),
+      ),
+    );
+  }
+}
+
+class _SnackbarView extends StatelessWidget {
+  const _SnackbarView();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text('Un mensaje en pantalla'),
+                  duration: const Duration(seconds: 2),
+                ),
+              );
+            },
+            child: const Text('Mostrar Snackbar'),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('Un diálogo'),
+                    content: const Text('Contenido del diálogo'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text('Cerrar'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            child: const Text('Mostrar Diálogo'),
+          ),
+        ],
+      ),
+    );
   }
 }
