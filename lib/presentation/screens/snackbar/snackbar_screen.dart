e23c8af8e09ccc041d3666ea7899019dc8eca9a2
OmarACY <omaralejandrocy@gmail.com>
2024-12-09 22:17:39 -0600

    Diálogos y Licencias

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/config/router/app_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
@@ -39,46 +41,47 @@ class SnackbarScreen extends StatelessWidget {
 class _SnackbarView extends StatelessWidget {
   const _SnackbarView();
 
+    void openDialog( BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('¿Estás seguro?'),
+          content: const Text('Incididunt dolore dolor sit esse ipsum. Quis ad dolore eiusmod quis sunt dolor. Deserunt anim ex cillum enim laboris veniam velit proident reprehenderit laboris. Officia amet ullamco irure id et id.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('Cerrar'),
+            ),
+            FilledButton(
+              onPressed: () => context.pop(),
+              child: const Text('Aceptar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          SizedBox(height: 30),
-          ElevatedButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: const Text('Un mensaje en pantalla'),
-                  duration: const Duration(seconds: 2),
-                ),
-              );
-            },
-            child: const Text('Mostrar Snackbar'),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    title: const Text('Un diálogo'),
-                    content: const Text('Contenido del diálogo'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.of(context).pop();
-                        },
-                        child: const Text('Cerrar'),
-                      ),
-                    ],
-                  );
-                },
-              );
-            },
-            child: const Text('Mostrar Diálogo'),
-          ),
+          FilledButton.tonal(onPressed:
+          () {
+            showAboutDialog(context: context,
+            children: const <Widget>[
+              Text('Este es un diálogo de prueba, consequat aliqua ex reprehenderit aute officia. Eiusmod dolore duis cillum fugiat ut amet magna. Quis ad proident cillum anim cillum laborum nisi ad occaecat occaecat labore culpa. Sunt aliquip cillum proident cupidatat officia deserunt. Ea incididunt pariatur reprehenderit labore non irure elit esse in proident. Exercitation tempor qui dolor ex laboris incididunt proident ex sint duis esse dolor. Officia culpa sint eu voluptate ipsum deserunt do.'),
+            ],
+            );
+          }, child: const Text('Licencias usadas')),
+          FilledButton.tonal(onPressed:
+          () {
+            openDialog(context);
+          }, child: const Text('Mostrar diálogo')),
         ],
       ),
     );
