0356fffc732bae5e04efb23af20aa46ac871095d
OmarACY <omaralejandrocy@gmail.com>
2024-12-11 00:27:14 -0600

    142. Checkbox, Radios y otros

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -16,4 +16,5 @@ const appMenuItems = <MenuItem>[
   MenuItem('Progress Indicators', 'Generales y controlados', '/progress', Icons.refresh_rounded),
   MenuItem('Snackbars y diálogos', 'Indicadores en pantalla', '/snackbars', Icons.info_outlined),
   MenuItem('Animated container', 'Stateful widget animado', '/animated', Icons.check_box_outline_blank_rounded),
+  MenuItem('UI Controls + Tiles', 'Una serie de controles de Flutter', '/ui-controls', Icons.car_rental_outlined),
 ];
--- lib/presentation/screens/ui_controls/ui_controls_screen.dart
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -1,13 +1,79 @@
 import 'package:flutter/material.dart';
 
 class UiControlsScreen extends StatelessWidget {
-
   static const String name = 'ui_controls_screen';
 
   const UiControlsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('UI Controls + Tiles'),
+      ),
+      body: const _UiControlsView(),
+    );
+  }
+}
+
+class _UiControlsView extends StatefulWidget {
+  const _UiControlsView();
+
+  @override
+  State<_UiControlsView> createState() => _UiControlsViewState();
+}
+
+enum Transportation { car, bike, plane, boat }
+
+class _UiControlsViewState extends State<_UiControlsView> {
+
+  bool isDeveloperMode = false;
+  Transportation selectedTransportation = Transportation.car;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const ClampingScrollPhysics(),
+      children: [
+        SwitchListTile(
+          title: const Text('Developer Mode'),
+          subtitle: const Text('Activar modo desarrollador'),
+          value: isDeveloperMode,
+          onChanged: (value) => setState(() => isDeveloperMode = !isDeveloperMode), 
+          ),
+          RadioListTile(
+            title: const Text('Car'),
+            subtitle: const Text('Vehículo terrestre'),
+            value: Transportation.car,
+            groupValue: selectedTransportation,
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.car;
+          })),
+          RadioListTile(
+            title: const Text('Biycle'),
+            subtitle: const Text('Vehículo terrestre'),
+            value: Transportation.bike,
+            groupValue: selectedTransportation,
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.bike;
+          })),
+          RadioListTile(
+            title: const Text('Plane'),
+            subtitle: const Text('Vehículo aéreo'),
+            value: Transportation.plane,
+            groupValue: selectedTransportation,
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.plane;
+          })),
+          RadioListTile(
+            title: const Text('Boat'),
+            subtitle: const Text('Vehículo acuático'),
+            value: Transportation.boat,
+            groupValue: selectedTransportation,
+            onChanged: (value) => setState(() {
+              selectedTransportation = Transportation.boat;
+          })),
+      ],
+    );
   }
 }
