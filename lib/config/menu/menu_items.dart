8dfbccf49185727b02b8997ee74bc6af5e706ee7
OmarACY <omaralejandrocy@gmail.com>
2024-12-16 22:40:30 -0600

    147. InfiniteScroll - Inicio

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -10,12 +10,12 @@ class MenuItem {
 }
 
 const appMenuItems = <MenuItem>[
-  MenuItem('Botones', 'Varios botones en Flutter', '/buttons',
-      Icons.smart_button_outlined),
+  MenuItem('Botones', 'Varios botones en Flutter', '/buttons',Icons.smart_button_outlined),
   MenuItem('Tarjetas', 'Un contenedor estilizado', '/cards', Icons.credit_card),
   MenuItem('Progress Indicators', 'Generales y controlados', '/progress', Icons.refresh_rounded),
   MenuItem('Snackbars y diálogos', 'Indicadores en pantalla', '/snackbars', Icons.info_outlined),
   MenuItem('Animated container', 'Stateful widget animado', '/animated', Icons.check_box_outline_blank_rounded),
   MenuItem('UI Controls + Tiles', 'Una serie de controles de Flutter', '/ui-controls', Icons.car_rental_outlined),
   MenuItem('Introduccion a la aplicacion', 'Pequeño tutorial introductorio', '/tutorial', Icons.accessible_rounded),
+  MenuItem('InfiniteScroll and Pull', 'Listas infinitas y pull to refresh', '/infinite', Icons.list_alt_rounded),
 ];
--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,13 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class InfiniteScrollScreen extends StatelessWidget {
-
+class InfiniteScrollScreen extends StatefulWidget {
   static const String name = 'infinite_scroll_screen';
 
   const InfiniteScrollScreen({super.key});
 
   @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        removeBottom: true,
+        child: ListView.builder(
+          //itemCount: 100,
+          itemBuilder: (context, index) {
+          return FadeInImage(
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 300,
+            placeholder: const AssetImage('assets/images/jar-loading.gif'),
+            image: NetworkImage('https://picsum.photos/500/300/?image=$index'),
+          );
+        }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+        //Navigator.pop(context);
+        context.pop();
+      },
+        child: const Icon(Icons.arrow_back),
+      ),
+    );
   }
 }
