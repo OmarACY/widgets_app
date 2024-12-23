58eb8aa8821f7bc90d1fd86e2d182bbf9854aa6a
OmarACY <omaralejandrocy@gmail.com>
2024-12-23 09:48:39 -0600

    157. Consideraciones de Notch y Opciones del menú

--- lib/presentation/widgets/side_menu.dart
+++ lib/presentation/widgets/side_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 class SideMenu extends StatefulWidget {
@@ -14,18 +15,41 @@ class _SideMenuState extends State<SideMenu> {
 
   @override
   Widget build(BuildContext context) {
+
+    final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
+
     return NavigationDrawer(
       selectedIndex: navDrawerIndex,
       onDestinationSelected: (value) => setState(() => navDrawerIndex = value),
       children: [
-        NavigationDrawerDestination(
-          icon: Icon(Icons.add),
-          label: Text('Home'),
+
+        Padding(
+          padding: EdgeInsets.fromLTRB(28, hasNotch ? 0 : 20,16,10),
+          child: const Text('Main')
         ),
-        NavigationDrawerDestination(
-          icon: Icon(Icons.add),
-          label: Text('Buttons'),
+
+        ...appMenuItems
+        .sublist(0, 3)
+        .map((item) => NavigationDrawerDestination(
+          icon: Icon(item.icon),
+          label: Text(item.title),
+        )),
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28, 16, 28, 10),
+          child: Divider(),
         ),
+
+        const Padding(
+          padding: EdgeInsets.fromLTRB(28,10,16,10),
+          child: Text('More options')
+        ),
+
+        ...appMenuItems
+        .sublist(3)
+        .map((item) => NavigationDrawerDestination(
+          icon: Icon(item.icon),
+          label: Text(item.title),
+        )),
     ],);
   }
 }
