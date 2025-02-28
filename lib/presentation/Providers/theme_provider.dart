fe0a4fbc91242e93798c27a00d25264d3af6fe9d
OmarACY <omaralejandrocy@gmail.com>
2025-02-27 19:49:13 -0600

    163. Pantalla para cambiar colores

--- lib/presentation/Providers/theme_provider.dart
+++ lib/presentation/Providers/theme_provider.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
 
+final colorListProvider = Provider((ref) => colorThemes);
+
 final isDarkModeProvider = StateProvider<bool>((ref) => false);
 
-final colorListProvider = Provider((ref) => colorThemes);
+final selectedColorProvider = StateProvider<int>((ref) => 0);
+
+//final selectedColorProvider = StateProvider<Color>((ref) => ref.watch(colorListProvider).first);
