3ff427cdba653cd36aadbcd0f3ac4a181433199a
OmarACY <omaralejandrocy@gmail.com>
2025-04-08 19:29:17 -0600

    165. Riverpod - StateNotifier

--- lib/presentation/Providers/theme_provider.dart
+++ lib/presentation/Providers/theme_provider.dart
@@ -6,5 +6,20 @@ final colorListProvider = Provider((ref) => colorThemes);
 final isDarkModeProvider = StateProvider<bool>((ref) => false);
 
 final selectedColorProvider = StateProvider<int>((ref) => 0);
-
 //final selectedColorProvider = StateProvider<Color>((ref) => ref.watch(colorListProvider).first);
+
+// Un objeto de tipo AppTheme (custom)
+final themeNotifierProvider = StateNotifierProvider<ThemeNotifier, AppTheme>(
+  (ref) => ThemeNotifier(),
+  );
+
+// Controller or notifier
+class ThemeNotifier extends StateNotifier<AppTheme> {
+
+  // State = new AppTheme
+  // Constructor
+  ThemeNotifier() : super( AppTheme());
+
+}
+
+
